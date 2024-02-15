dbfe3ac210a48a6491c24b96723ef26d5c4db7fb
juan sebastian <juan.quiroz@gse.com.co>
2024-02-15 12:20:43 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import React, { useState } from 'react';
-
-import './App.css';
-
 import { QrReader } from 'react-qr-reader';
 
 function App() {
 
-  const [result, setResult] = useState('');
-
-  const handleScan = (data) => {
-    if (data) {
-      setResult(data);
-    }
-  }
-
-  const handleError = (err) => {
-    console.error(err);
-  }
+  const [data, setData] = useState('No result');
 
   return (
     <>
-      <div>
-        <QrReader
-          delay={300}
-          onError={handleError}
-          onScan={handleScan}
-          style={{ width: '100%' }}
-        />
-        <p>{result}</p>
-      </div>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      <p>{data}</p>
     </>
   );
 }
