0e0a50a4bc7f5ca20077ab45138c62a5ed96edac
juan sebastian <juan.quiroz@gse.com.co>
2024-02-19 16:11:57 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Html5QrcodeScanner, Html5Qrcode } from 'html5-qrcode';
+import { Html5Qrcode } from 'html5-qrcode';
 
 function App() {
 
@@ -32,7 +32,7 @@ function App() {
       // console.log(decodedResult);
         /* handle success */
     };
-    const config = { fps: 10, qrbox: { width: 250, height: 50 } };
+    const config = { fps: 10, qrbox: { width: 350, height: 250 } };
 
     // If you want to prefer back camera
     html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
