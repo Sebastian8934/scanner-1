5d53f47eefa2892514d813ee736ec57c29bbb3e5
juan sebastian <juan.quiroz@gse.com.co>
2024-02-21 16:46:42 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Html5Qrcode } from 'html5-qrcode';
+import { useZxing } from "react-zxing";
 
 function App() {
-
-  const [result, setResult] = useState();
-
-  // useEffect(() => {
-  //   function onScanSuccess(decodedText, decodedResult) {
-  //     // handle the scanned code as you like, for example:
-  //     console.log(`Code matched = ${decodedText}`, decodedResult);
-  //   }
-    
-  //   function onScanFailure(error) {
-  //     // handle scan failure, usually better to ignore and keep scanning.
-  //     // for example:
-  //     console.warn(`Code scan error = ${error}`);
-  //   }
-    
-  //   let html5QrcodeScanner = new Html5QrcodeScanner(
-  //     "reader",
-  //     { fps: 10, qrbox: {width: 250, height: 250} },
-  //     /* verbose= */ false);
-  //   html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-  // });
-
-  useEffect(() => {
-    const html5QrCode = new Html5Qrcode("reader");
-    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-      setResult(decodedText)
-      // console.log(decodedText);
-      // console.log(decodedResult);
-        /* handle success */
-    };
-    const config = { fps: 80, qrbox: { width: 350, height: 100 } };
-
-    // If you want to prefer back camera
-    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
-  }, [])
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    },
+  });
   
   return (
     <>
-      <div id="reader" width="600px"></div>
-      <div>
-        {result}
-      </div>
+      <video ref={ref} />
+      <p>
+        <span>Last result:</span>
+        <span>{result}</span>
+      </p>
     </>
   );
 }
